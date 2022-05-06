144633323c7b2acfac867b8bdc6a4c0a82e4ed0d
qsewell <103156726+qsewell@users.noreply.github.com>
2022-05-06 11:26:58 -0700

    Makefile Working

---
+++ MarkdownParseTest.java
@@ -0,0 +1,58 @@
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.*;
+
+public class MarkdownParseTest {
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+    @Test
+    public void getLinks() throws IOException {
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("https://something.com");
+        expected.add("some-thing.html");
+        assertEquals(expected, MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void getLinks2() throws IOException {
+        Path fileName = Path.of("test-file2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("https://www.microsoft.com/en-us/store/apps/windows");
+        assertEquals(expected, MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void getLinks3() throws IOException {
+        Path fileName = Path.of("test-file3.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("https://something.com");
+        assertEquals(expected, MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void getLinks4() throws IOException {
+        Path fileName = Path.of("test-file4.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> expected = new ArrayList<>();
+        boolean exceptionThrown = false;
+        try {
+            MarkdownParse.getLinks(content);
+        }
+        catch (IOException e) {
+            exceptionThrown = true;
+        }
+        assertTrue("Except thrown for NullPointerException", 
+            exceptionThrown); 
+    }
+}
---
+++ test-file2.md
@@ -0,0 +1,4 @@
+# Test 2
+
+[Link1](https://www.microsoft.com/en-us/store/apps/windows)
+![image](test.jpg)
